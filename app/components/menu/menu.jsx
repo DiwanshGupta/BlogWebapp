c99c3731bdd474f69d9b61ce1ba6a628299e6a51
Diwansh Gupta <130465365+DiwanshGupta@users.noreply.github.com>
2023-10-19 11:39:11 +0530

    Update menu.jsx

--- app/components/menu/menu.jsx
+++ app/components/menu/menu.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Menu() {
   return (
-      <div className="py-8 px-1">
+    <div className="py-8 px-1">
       <div>What's Hot</div>
       <div className="font-semibold text-xl">Most Popular</div>
       <div>
@@ -11,58 +11,57 @@ function Menu() {
         <div className="">
           <div className="flex-1 px-3 py-4">
             <span className="font-semibold text-white border border-white bg-red-500 rounded-3xl px-2 ">
-              <Link href={`/Blog?cat=Travel&apos;`}> Travel</Link>
+              <Link href={`/Blog?cat=Travel`}> Travel</Link>
             </span>
             <div className="flex font-medium text-slate-600 ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </div>
             <span className="font-semibold text-base">
-              Diwansh Gupta &apos;<span className="text-slate-600">11.2.2003</span>
+              Diwansh Gupta -<span className="text-slate-600">11.2.2003</span>
             </span>
           </div>
         </div>
         <div className="">
           <div className="flex-1 px-3 py-4">
             <span className="font-semibold text-white border border-white bg-yellow-400 rounded-3xl px-2 ">
-              <Link href={`/Blog?cat=Culture&apos;`}> Culture</Link>
+              <Link href={`/Blog?cat=Culture`}> Culture</Link>
             </span>
             <div className="flex font-medium text-slate-600 ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </div>
             <span className="font-semibold text-base">
-              Diwansh Gupta &apos;<span className="text-slate-600">11.2.2003</span>
+              Diwansh Gupta -<span className="text-slate-600">11.2.2003</span>
             </span>
           </div>
         </div>
         <div className="">
           <div className="flex-1 px-3 py-4">
             <span className="font-semibold text-white border border-white bg-blue-400 rounded-3xl px-2 ">
-              <Link href={`/Blog?cat=Style&apos;`}> Style</Link>
+              <Link href={`/Blog?cat=Style`}> Style</Link>
             </span>
             <div className="flex font-medium text-slate-600 ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </div>
             <span className="font-semibold text-base">
-              Diwansh Gupta &apos;<span className="text-slate-600">11.2.2003</span>
+              Diwansh Gupta -<span className="text-slate-600">11.2.2003</span>
             </span>
           </div>
         </div>{" "}
         <div className="">
           <div className="flex-1 px-3 py-4">
             <span className="font-semibold text-white border border-white bg-orange-600 rounded-3xl px-2 ">
-              <Link href={`/Blog?cat=Food&apos;`}> Food</Link>
+              <Link href={`/Blog?cat=Food`}> Food</Link>
             </span>
             <div className="flex font-medium text-slate-600 ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </div>
             <span className="font-semibold text-base">
-              Diwansh Gupta &apos;<span className="text-slate-600">11.2.2003</span>
+              Diwansh Gupta -<span className="text-slate-600">11.2.2003</span>
             </span>
           </div>
         </div>
       </div>
     </div>
-
   );
 }
 
